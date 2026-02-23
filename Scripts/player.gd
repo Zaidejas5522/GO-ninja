cb3eb6e54a1c2d265349dbef2efa0b5508316c64
Zaidejas5522 <kristupasmeskauskas@gmail.com>
2026-02-23 16:32:17 +0200

    Attack animation input
    Right animation and timing for attack

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -10,16 +10,57 @@ var right_pressed := false
 var up_pressed := false
 var down_pressed := false
 
+var attacking = 0
+
 #Track last pressed direction
 enum Direction { NONE, LEFT, RIGHT, UP, DOWN }
-var last_direction := Direction.NONE
+var last_direction := Direction.NONE # Last pressed direction for movement
+var facing_direction := Direction.NONE  # Direction the player is facing for idle/attack
 
 
 func _ready() -> void:
-	pass
+	facing_direction = Direction.DOWN
 
 func _process(delta: float) -> void:
-	_movement(delta) #The movement script is not final might change
+	if Input.is_action_just_pressed("Attack") and not attacking:
+		start_attack()
+	
+	if attacking:
+		# When attacking, don't move and play attack animation
+		velocity = Vector2.ZERO
+		match facing_direction:
+			Direction.LEFT:
+				sprite.play("AttackWest")
+			Direction.RIGHT:
+				sprite.play("AttackEast")
+			Direction.UP:
+				sprite.play("AttackNorth")
+			Direction.DOWN:
+				sprite.play("AttackSouth")
+			Direction.NONE:
+				sprite.play("AttackSouth") # default if no direction
+	else:
+		_movement(delta)
+
+func start_attack():
+	attacking = true
+	print("Attack started")
+	await get_tree().create_timer(0.3).timeout # attack timer, can modify
+	print("Attack finished")
+	attacking = false
+	# After attack finishes, play the appropriate idle animation
+	if not (left_pressed or right_pressed or up_pressed or down_pressed):
+		match facing_direction:
+			Direction.LEFT:
+				sprite.play("IdleWest")
+			Direction.RIGHT:
+				sprite.play("IdleEast")
+			Direction.UP:
+				sprite.play("IdleNorth")
+			Direction.DOWN:
+				sprite.play("IdleSouth")
+			Direction.NONE:
+				sprite.play("IdleSouth")
 	
 func _physics_process(delta: float) -> void:
 	move_and_slide()
@@ -31,16 +72,21 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_left"):
 		left_pressed = true
 		last_direction = Direction.LEFT
+		facing_direction = Direction.LEFT  
 	elif event.is_action_pressed("ui_right"):
 		right_pressed = true
 		last_direction = Direction.RIGHT
+		facing_direction = Direction.RIGHT  
 	elif event.is_action_pressed("ui_up"):
 		up_pressed = true
 		last_direction = Direction.UP
+		facing_direction = Direction.UP  
 	elif event.is_action_pressed("ui_down"):
 		down_pressed = true
 		last_direction = Direction.DOWN
-	
+		facing_direction = Direction.DOWN  
+		
+		
 	# Track key releases
 	if event.is_action_released("ui_left"):
 		left_pressed = false
@@ -59,18 +105,23 @@ func _update_last_direction():
 	# Check pressed keys in priority order
 	if right_pressed:
 		last_direction = Direction.RIGHT
+		facing_direction = Direction.RIGHT  
 	elif left_pressed:
 		last_direction = Direction.LEFT
+		facing_direction = Direction.LEFT  
 	elif up_pressed:
 		last_direction = Direction.UP
+		facing_direction = Direction.UP  
 	elif down_pressed:
 		last_direction = Direction.DOWN
+		facing_direction = Direction.DOWN  
+		
 func _movement(delta):
 	velocity = Vector2.ZERO
 	
 	#check if any of the keys are pressed before moving
 	if not (left_pressed or right_pressed or up_pressed or down_pressed):
-		match last_direction:
+		match facing_direction:
 			Direction.LEFT:
 				sprite.play("IdleEast")
 			Direction.RIGHT:
@@ -79,7 +130,6 @@ func _movement(delta):
 				sprite.play("IdleNorth")
 			Direction.DOWN:
 				sprite.play("IdleSouth")
-		
 		last_direction = Direction.NONE
 		return
 	
