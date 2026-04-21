fc2ab0084ed0a00a9136730e3c8f64404011650b
Zaidejas5522 <kristupasmeskauskas@gmail.com>
2026-04-21 17:21:31 +0300

    a

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var PlayerHealth = 99999
+var PlayerHealth = 30
 var IsDamagable = false
 var OtherAttacking = false
 var Money = 0
@@ -8,7 +8,7 @@ var Money = 0
 
 
 #POTENTIAL STUFF IS IN WEAPON SCRIPT WHEN A BODY ENTERS AREA 2D AND CURRENT IS IN PROCESS METHOD
-var MaxPlayerHealth = 9999999
+var MaxPlayerHealth = 30
 var PotentialPlayerHealth = 0
 var CurrentItemHealth = 0
 
@@ -16,7 +16,7 @@ var PlayerSpeed = 130
 var PotentialPlayerSpeed = 0
 var CurrentItemSpeed = 0
 
-var PlayerDamage = 300
+var PlayerDamage = 10
 var PotentialPlayerDamage = 0
 var CurrentItemDamage = 0
 
